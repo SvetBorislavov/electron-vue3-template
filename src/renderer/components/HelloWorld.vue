b742820a634899a316921067e132d2a9ac26ad9d
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-11-17 15:11:13 +0200

    feat: create Pinia example

--- src/renderer/components/HelloWorld.vue
+++ src/renderer/components/HelloWorld.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
+import { useCounterStore } from '../stores/storeCount';
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const storeCount = useCounterStore();
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button
+      type="button"
+      @click="storeCount.increment()"
+    >
+      count is {{ storeCount.count }}
+    </button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -20,7 +26,11 @@ const count = ref(0)
   </div>
   <p>
     Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <a
+      href="https://github.com/johnsoncodehk/volar"
+      target="_blank"
+      >Volar</a
+    >
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
